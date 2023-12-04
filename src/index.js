41419242a0e706b261bd4fe1a5cc81904dee5fec
ViNu-23 <vijayvinu46@gmail.com>
2023-12-04 23:26:39 +0530

    new app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 import  ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
-
 function Main() {
   return (
     <>
@@ -11,8 +9,6 @@ function Main() {
     </>
   );
 }
-
-const root = document.getElementById("root");
-const rootContainer= ReactDOM.createRoot(root);
-rootContainer.render(<Main/>)
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main/>)
 
